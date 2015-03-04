59265731226fea41fb4af7c274986ceab264aadf
Simon Pedersen <blasimtex@hotmail.com>
2015-03-05 00:02:28 +0100

    init

---
+++ SJKP.AzureSearch.PdfIndexer/Functions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace SJKP.AzureSearch.PdfIndexer
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([BlobTrigger("documents/{name}.{ext}")] Stream input, string name, string ext)
+        {
+            if (ext.ToLower() != "pdf")
+            {
+                return;
+            }
+            List<IndexAction> actions = new List<IndexAction>();
+
+            using (PdfReader reader = new PdfReader(input))
+            {
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    actions.Add(new IndexAction(IndexActionType.MergeOrUpload, new Document()
+                    {
+                        {"filename",name+"."+ext } ,
+                        {"id",MakeSafeId(name+"_"+i+ "."+ext)},
+                        {"content", PdfTextExtractor.GetTextFromPage(reader, i) },
+                        {"page", i }
+                    }));
+                }
+            }
+
+            var client = new SearchIndexClient("sjkp", "documents", new SearchCredentials(ConfigurationManager.AppSettings["SearchApiKey"]));
+
+            
+            for (int i = 0; i < (int)Math.Ceiling(actions.Count / 1000.0); i++)
+            {
+                client.Documents.Index(new IndexBatch(actions.Skip(i*1000).Take(actions.Count-(i*1000))));
+            }
+            
+        }
+
+        private static string MakeSafeId(string input) {
+            Regex rgx = new Regex("[^a-zA-Z0-9_]");
+            return rgx.Replace(input, "");           
+
+        }
+    }
+}
---
+++ SJKP.AzureSearch.PdfIndexer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System.Configuration;
+
+namespace SJKP.AzureSearch.PdfIndexer
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            Setup();
+            var host = new JobHost();
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+
+        private static void Setup()
+        {
+            var searchClient = new SearchServiceClient("sjkp", new SearchCredentials(ConfigurationManager.AppSettings["SearchApiKey"]));
+            var indexName = "documents";
+            var indexExists = searchClient.Indexes.ListNames().Any(s => s == indexName);
+            if (indexExists)
+            {
+                Console.WriteLine("Index {0} exists", indexName);
+                return;
+            }
+                    
+
+            var task = searchClient.Indexes.CreateAsync(new Microsoft.Azure.Search.Models.Index()
+            {
+                Name = indexName,
+                Fields = new Field[] {
+                        new Field("filename", DataType.String, AnalyzerName.EnLucene) {
+                            IsRetrievable = true,
+                            IsSearchable = true,
+                        },
+                        new Field("content", DataType.String, AnalyzerName.EnLucene) {
+                            IsSearchable = true,
+                        },
+                        new Field("id", DataType.String) {
+                            IsKey = true,
+                        },
+                        new Field("page", DataType.Int32) {
+                            IsSortable = true,                            
+                            IsFilterable = true
+                        }
+                    }
+
+
+            });
+
+            Task.WaitAll(task);
+            Console.WriteLine("Result of index creation: " + task.Result.StatusCode);
+        }
+    }
+}
